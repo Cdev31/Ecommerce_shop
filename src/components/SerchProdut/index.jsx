0753d0a1e8985f4cc1f1df388f6522f45f72a999
kalet chavez <kalet.svdev@gmail.com>
2023-08-25 22:20:05 -0600

    ultimas eliminaciones

--- src/components/SerchProdut/index.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export const SechProduct = ()=>{
-    return (
-        <div className="flex mt-2 col-start-2 col-end-4 row-star-2 ">
-            <input className="w-full  border-b border-black/50 h-10  placeholder:text-black/90 font-medium text-[16px] pl-1 focus:outline-none text-black " placeholder="Serch . . ." type="text"/>
-        </div>
-    )
-}
